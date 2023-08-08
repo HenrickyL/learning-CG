b88c7dd04c13dfde41129053a62916016809734b
HenrickyL <Henrickyl1@gmail.com>
2023-08-08 06:47:41 -0300

    windows config

--- DXUT/DXUT/main.cpp
+++ DXUT/DXUT/main.cpp
@@ -2,5 +2,23 @@
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+	// configura e cria a janela
+	Window* window = new Window();
+
+	window->Mode(WINDOWED);
+	window->Size(1024, 600);
+	window->Color(0, 122, 204);
+	window->Title("Aplicação");
+	window->Icon(IDI_ICON);
+	window->Cursor(IDC_CURSOR);
+	window->Create();
+
+	// trata as mensagens destinadas a janela
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 }
