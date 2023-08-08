6b95f3eceb3766a4b54cbd635c7bd36ffcc45a18
HenrickyL <Henrickyl1@gmail.com>
2023-08-08 06:26:01 -0300

    add base headers & main

---
+++ DXUT/DXUT/Window.h
@@ -0,0 +1,12 @@
+#ifndef DXUT_WINDOW_H
+#define DXUT_WINDOW_H
+
+// includes
+
+#include "Types.h"      // tipos personalizados da biblioteca
+#include <windows.h>    // inclui funções do windows
+#include <windowsx.h>   // inclui extensões do windows
+#include <string>       // inclui a classe string
+using std::string;      // permite usar o tipo string sem std::
+
+#endif
